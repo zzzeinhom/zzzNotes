19756ca9006a819bcc5b6eccc289e0f01c1d47f4
zzzeinhom <mohamedzenhom200@gmail.com>
2025-01-31 20:45:24 +0200

    Refactor NoteView and NotesViewBody to integrate NotesCubit with BlocProvider for improved state management.

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zzznotes/cubits/notes_cubit/notes_cubit.dart';
 import 'package:zzznotes/views/widgets/notes_list.dart';
 import 'package:zzznotes/views/widgets/notes_app_bar.dart';
 
@@ -7,13 +9,16 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 24, 16, 8),
-      child: Column(
-        children: [
-          NotesAppBar(),
-          NotesList(),
-        ],
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 24, 16, 8),
+        child: Column(
+          children: [
+            NotesAppBar(),
+            NotesList(),
+          ],
+        ),
       ),
     );
   }
