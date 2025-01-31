a1444a7496ee851427444777fa4e9ae0a5ac2cc6
zzzeinhom <mohamedzenhom200@gmail.com>
2025-01-31 12:00:12 +0200

    Initialize Hive in main function and open 'notes' box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:zzznotes/views/notes_view.dart';
 import 'package:zzznotes/themes/themes.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox('notes');
   runApp(const ZZZNaotes());
 }
 
